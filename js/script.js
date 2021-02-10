1a9ebb597327a593cb6e07f9163bb0e27e8ae415
wlsakfldk <dhfosw20000@naver.com>
2021-02-10 14:26:26 +0900

    commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function($) {
+
+$(".scroll").click(function(event){            
+
+event.preventDefault();
+
+$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+
+});
+
+});
+
